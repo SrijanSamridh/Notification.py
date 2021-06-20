6ce838e80dde8ab1dd68d8b8d94520b3480dc417
Srijan Samridh <74289654+SrijanSamridh@users.noreply.github.com>
2021-06-20 12:07:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                     "Basically, every 20 minutes spent using a screen;"
                     " you should try to look away at something that is "
                     "20 feet away from you for a total of 20 seconds.",
-            # app_icon="C:\Users\Srijan Samridh\Desktop\Python\Notification Reminder App\icon.ico",
+            app_icon="C:\Users\Srijan Samridh\Desktop\Python\Notification Reminder App\icon.ico",
             app_name="Eye Alert!",
             timeout=2
         )
